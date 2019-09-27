8b1782f5b784bd9badfd998b5a37ecf6176c3d73
Eli-Brown <vincent.brown@seattlecolleges.edu>
2019-09-26 20:48:03 -0700

    Create apache-script-.sh

---
+++ apache-script-.sh
@@ -0,0 +1,7 @@
+
+yum -y install httpd mod ssl                                                           # start apache commit out welcome page
+systemctl restart httpd                                                                # restart httpd service
+sed -i 's/^/#/g' /etc/httpd.conf.d/welcome.conf                                        # create webpage 
+echo "<html><body><H1>Hi There NTI-300</h1></body></html>" > /var/www/html/index.html
+systemctl restart httpd
+

b3d4ab74ad48a189811285016e2535a366a1a9e9
Eli <vincent.brown@seattlecolleges.edu>
2019-10-24 19:53:40 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,39 +1,17 @@
-#!bash
- sudo yum install python-django
-     django-admin --version
-      sudo yum install python-pip
-     sudo yum install django
-      sudo yum install django
-      sudo yum install python-pip
-      sudo pip install virtualenv
-      sudo pip install upgrade
-      sudo pip install --upgrade pip
-     mkdir ~/newproject
-     cd ~/newproject
-     virtualenv newenv
-     source newenv/bin/activate
-     pip install django
-     django-admin startproject newproject
-     cd ~/newproject 
-     python manage.py migrate
-    
-    sed- i "search for allowed host" 
-    
-    
-    
-   31  python manage.py createsuperuser
-   32  python manage.py createsuperuser
-   34  django-admin startproject newproject
+#!/bin/bash
 
-   36  django-admin startproject newproject
-   37  python manage.py createsuperuser
-   38  python manage.py runserver 0.0.0.0:8000
-   39  grep -r "ALLOWED_HOSTS"
-
-   41  vim newproject/settings.py
-   42  django-admin startproject newproject
-   27  python manage.py migrate
-   28  django-admin startproject newproject/
-
-
-   51  python manage.py runserver 0.0.0.0:8000
+yum install -y python-pip
+pip install virtualenv   
+pip install --upgrade pip  
+mkdir ~/newproject    
+cd ~/newproject/    
+virtualenv newenv      
+source newenv/bin/activate    
+pip install django   
+django-admin --version      
+django-admin startproject newproject
+cd newproject/
+python manage.py migrate
+python manage.py createsuperuser         # <-work around this to add a user
+newproject/settings.py			                # sed search and replace
+python manage.py runserver 0.0.0.0:8000

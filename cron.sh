79edbacc1e67454e32b29158f7c0fefcf3dc46b3
Eli <vincent.brown@seattlecolleges.edu>
2019-10-29 14:54:10 -0700

    Updated file with changes

--- cron.sh
+++ cron.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
-   if [ `id -u` -ne 0 ]; then
-
-      echo "This script can be executed only with root, Exiting.."
+   if [ `id -u` -ne 0 ]; then																#
+																						
+      echo "This script can only be executed only with root powers, Now exiting.."			#
       exit 1
    fi
 
-case "$1" in
-   install|update)
+		case "$1" in																		#
+			install|update)
 
-	CRON_FILE="/var/spool/cron/root"
+			Cron_File="/var/spool/cron/root"
 
-	if [ ! -f $CRON_FILE ]; then
-	   echo "cron file for root doesnot exist, creating.."
-	   touch $CRON_FILE
-	   /usr/bin/crontab $CRON_FILE
-	fi
+			if [ ! -f $Cron_File ]; then
+				echo "cron file for root does not exist, creating.."
+				touch $Cron_File
+				/usr/bin/crontab $Cron_File
+			fi
 
-	# Method 1
-	grep -qi "cleanup_script" $CRON_FILE
-	if [ $? != 0 ]; then
-	   echo "Updating cron job for cleaning temporary files"
-           /bin/echo "0 0 * * * rm -f /home/eli/cleanup_script.sh" >> $CRON_FILE
-	fi
+			
+		grep -qi "cleanup_script" $Cron_File
+			if [ $? != 0 ]; then
+				echo "Updating cron job for cleaning temporary files"
+				/bin/echo "0 0 * * * rm -f /home/eli/cleanup_script.sh" >> $Cron_File
+			fi
 
-	# Method 2
-	grep -qi "cleanup_script" $CRON_FILE
-	if [ $? != 0 ]; then
-	   echo "Updating cron job for cleaning temporary files"
-	   crontab -u eli -l >/tmp/crontab
-           /bin/echo "0 0 * * * rm -f /home/eli/cleanup_script.sh" >> /tmp/crontab
-	   crontab -u eli /tmp/crontab
-	fi
 
+		grep -qi "cleanup_script" $Cron_File												#
+			if [ $? != 0 ]; then															#
+				echo "Updating cron job for cleaning temporary files"						#
+				crontab -u eli -l >/tmp/crontab												#
+				/bin/echo "0 0 * * * rm -f /home/eli/cleanup_script.sh" >> /tmp/crontab 	#
+				crontab -u eli /tmp/crontab													#
+			fi
 	;;
-	
 	*)
-	
 	echo "Usage: $0 {install|update}"
 	exit 1
     ;;

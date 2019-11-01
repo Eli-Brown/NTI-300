32e257503d0356edfdbcbec68895c6c4567ae2cb
Eli <vincent.brown@seattlecolleges.edu>
2019-10-31 17:07:42 -0700

    moved my git before loop

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/Eli-Brown/NTI-300/master/packages.txt
+
 for packages in $(cat packages.txt); do
   yum -y install $packages
-wget https://raw.githubusercontent.com/Eli-Brown/NTI-300/master/packages.txt
 done

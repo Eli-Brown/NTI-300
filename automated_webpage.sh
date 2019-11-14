ec3d2abdaa44d08644212eebb57cd0700d3d38c0
Eli <vincent.brown@seattlecolleges.edu>
2019-11-14 15:39:34 -0800

    Update automated_webpage.sh
    updating time on webpage

--- automated_webpage.sh
+++ automated_webpage.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 
+time=$(date)                                                                                                        # change variable time equal to date 
+echo "<html><body><h1>Hello NTI-300, it is $time, Are we learning yet?</h1></body></html>" > /var/www/html/pages.dir/page2.html  # Update page2 date/time

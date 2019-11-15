5e67b469c80d77ff978634e4e4d728bc1d2c2be0
Eli <vincent.brown@seattlecolleges.edu>
2019-11-14 16:20:56 -0800

    Update django-install-script.sh

--- django-install-script.sh
+++ django-install-script.sh
@@ -5,23 +5,16 @@ pip install virtualenv
 pip install --upgrade pip
 sudo mkdir /opt/django
 cd /opt/django
-
 yum install epel-release -y
 yum install python34 python-pip -y
 #Upgrades python.
-
 virtualenv -p python3 django
 cd django
 source bin/activate
-
 pip install django
 django-admin startproject project1
-
 cd ..
 yum install git -y
-
-
-
 sudo -u eli
 virtualenv -p python3 django
 cd opt/django/django

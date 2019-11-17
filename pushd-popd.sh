04818d13cc24375df8dbc76251100231fc509069
Eli <vincent.brown@seattlecolleges.edu>
2019-11-16 16:46:41 -0800

    1 upload two  bash exercises to github
    Chapter 4 of Learning the Bash Shell and upload two exercises to github

--- pushd-popd.sh
+++
@@ -1,1 +0,0 @@
-pushd ( ){dirname=$1DIR_STACK="$dirname ${DIR_STACK:-$PWD' '}"cd ${dirname:?"missing directory name."}echo "$DIR_STACK"}popd ( ){DIR_STACK=${DIR_STACK#* }cd ${DIR_STACK%% *}echo "$PWD"}

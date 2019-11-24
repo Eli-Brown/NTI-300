864105c078354fa56d7ed4c02583c2af39c67c1b
Eli <vincent.brown@seattlecolleges.edu>
2019-11-24 14:35:03 -0800

    Update python_django.py

--- python_django.py
+++ python_django.py
@@ -16,7 +16,7 @@ compute = discovery.build('compute', 'v1', credentials=credentials)
 
 project = 'cogent-genre-254202'
 zone = 'us-west1-a'
-name = 'go-django'
+name = 'go-django2'
 
 def list_instances(compute, project, zone):
   result = compute.instances().list(project=project, zone=zone).execute()

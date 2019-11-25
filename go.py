f9dc063eb3ff587f3da75ca209acf250b8c6e0ab
Eli <vincent.brown@seattlecolleges.edu>
2019-11-25 13:37:32 -0800

    Update go.py

--- go.py
+++ go.py
@@ -15,7 +15,7 @@ compute = discovery.build('compute', 'v1', credentials=credentials)
 
 project = 'cogent-genre-254202'
 zone = 'us-west1-a'
-name = 'GO'
+name = '-go-'
 
 def list_instances(compute, project, zone):
   result = compute.instances().list(project=project, zone=zone).execute()

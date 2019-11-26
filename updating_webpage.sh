83aedda0fa4b8bc2bc3248959dfe5e954ad261eb
Eli <vincent.brown@seattlecolleges.edu>
2019-11-26 11:50:43 -0800

    Update updating_webpage.sh

--- updating_webpage.sh
+++ updating_webpage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash                 
 
 time=$(date)                #sets variable time
-echo "<html><body><h1>Hello there NTI-300, it is now: $time, how are you doing today?</h1></body></html>" > /var/www/html/index.html  #create html page with time var
+echo "<html><body><h1>Hello there NTI-300, it is now: $time, What would you like to do today?</h1></body></html>" > /var/www/html/index.html  #create html page with time var
